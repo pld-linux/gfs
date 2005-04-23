372d96550a2174293ddd3a5a8093d8708a85406b
Jakub Bogusz <qboosh@pld-linux.org>
2005-04-23 19:20:57 +0000

    fad0a58f6f39499661704f0d5af3a8c0  gfs_ioctl.h 772e1801249bff0478b844924c0fdc20  gfs_ondisk.h 5b000a3b33af218e1b6b8a7d96b7e356  lm_interface.h
    Changed files:
        gfs_ioctl.h -> 1.1
        gfs_ondisk.h -> 1.1
        lm_interface.h -> 1.1

---
+++ gfs_ioctl.h
@@ -0,0 +1,33 @@
+/******************************************************************************
+*******************************************************************************
+**
+**  Copyright (C) Sistina Software, Inc.  1997-2003  All rights reserved.
+**  Copyright (C) 2004 Red Hat, Inc.  All rights reserved.
+**
+**  This copyrighted material is made available to anyone wishing to use,
+**  modify, copy, or redistribute it subject to the terms and conditions
+**  of the GNU General Public License v.2.
+**
+*******************************************************************************
+******************************************************************************/
+
+#ifndef __GFS_IOCTL_DOT_H__
+#define __GFS_IOCTL_DOT_H__
+
+#define _GFSC_(x)               (('G' << 8) | (x))
+
+/* Ioctls implemented */
+
+#define GFS_IOCTL_IDENTIFY      _GFSC_(35)
+#define GFS_IOCTL_SUPER         _GFSC_(45)
+
+struct gfs_ioctl {
+	unsigned int gi_argc;
+	char **gi_argv;
+
+        char __user *gi_data;
+	unsigned int gi_size;
+	uint64_t gi_offset;
+};
+
+#endif /* ___GFS_IOCTL_DOT_H__ */
